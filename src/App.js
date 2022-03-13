99c0031759e378c2a37433a970027d2279800067
Eduardo Kuwakino <kuwakino@gmail.com>
2022-03-13 12:58:43 -0300

    created react app;

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import bandeira from './assets/bandeira-nacional-brasil.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <img src={bandeira} alt="bandeira"/>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
