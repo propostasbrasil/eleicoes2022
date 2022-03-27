377bc9426a3aecc23ac8fb05250cbfc02ed9a404
Eduardo Kuwakino <kuwakino@gmail.com>
2022-03-27 13:16:45 -0300

    feat(calendar): layout adjustments;

--- src/components/dashboard/Calendar.js
+++ src/components/dashboard/Calendar.js
@@ -9,7 +9,7 @@ const Calendar = () => {
           Principais datas - Eleições 2022
         </CardSubtitle>
         <div>
-            <iframe width={800} height ={600} src={"data:text/html,"+encodeURIComponent('<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FSao_Paulo&showNav=1&showCalendars=1&mode=AGENDA&src=amJzZDdkNnUwbmtzZWN2YTVnZWN2b2c1b2dAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=Y2tjaDI0OTkydWZjaWpqdmdlNjU4NmRkbjJnZ2phdnJAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23D81B60&color=%239E69AF" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>')}/>
+            <iframe width={1000} height ={640} src={"data:text/html,"+encodeURIComponent('<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FSao_Paulo&showNav=1&showCalendars=1&mode=AGENDA&src=amJzZDdkNnUwbmtzZWN2YTVnZWN2b2c1b2dAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=Y2tjaDI0OTkydWZjaWpqdmdlNjU4NmRkbjJnZ2phdnJAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23D81B60&color=%239E69AF" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>')}/>
         </div>
       </CardBody>
     </Card>
